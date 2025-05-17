c84da19ba8647bec4184c7c54389bc0377de7342
Ashwathprabhu-108 <ashwathprabhu962@gmail.com>
2025-05-17 11:11:46 +0530

    Initial commit

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -14,8 +14,6 @@ const Footer = () => {
         </div>
         <ul className="footer-links">
             <li>Company</li>
-            <li>Products</li>
-            <li>Offices</li>
             <li>About</li>
             <li>Contact</li>
         </ul>
